5c92a19f7a681c066f12bfb1dc71ddacd7d80bcc
rjsilver <75267186+rjsilver@users.noreply.github.com>
2020-12-21 23:48:28 -0700

    Adding spi1 interface

--- projects/adrv9009zu11eg/adrv2crr_fmc/system_bd.tcl
+++ projects/adrv9009zu11eg/adrv2crr_fmc/system_bd.tcl
@@ -2,6 +2,27 @@
 source ../common/adrv9009zu11eg_bd.tcl
 source ../common/adrv2crr_fmc_bd.tcl
 
+create_bd_port -dir O -from 2 -to 0 spi1_csn
+create_bd_port -dir O spi1_sclk
+create_bd_port -dir O spi1_mosi
+create_bd_port -dir I spi1_miso
+
+set_property -dict [list CONFIG.PSU__SPI1__PERIPHERAL__ENABLE {1} CONFIG.PSU__SPI1__PERIPHERAL__IO {EMIO}] [get_bd_cells sys_ps8]
+set_property -dict [list CONFIG.PSU__SPI1__GRP_SS1__ENABLE {1} CONFIG.PSU__SPI1__GRP_SS2__ENABLE {1}] [get_bd_cells sys_ps8]
+
+ad_ip_instance xlconcat spi1_csn_concat
+ad_ip_parameter spi1_csn_concat CONFIG.NUM_PORTS 3
+ad_connect  sys_ps8/emio_spi1_ss_o_n spi1_csn_concat/In0
+ad_connect  sys_ps8/emio_spi1_ss1_o_n spi1_csn_concat/In1
+ad_connect  sys_ps8/emio_spi1_ss2_o_n spi1_csn_concat/In2
+ad_connect  spi1_csn_concat/dout spi1_csn
+ad_connect  sys_ps8/emio_spi1_sclk_o spi1_sclk
+ad_connect  sys_ps8/emio_spi1_m_o spi1_mosi
+ad_connect  sys_ps8/emio_spi1_m_i spi1_miso
+ad_connect  sys_ps8/emio_spi1_ss_i_n VCC
+ad_connect  sys_ps8/emio_spi1_sclk_i GND
+ad_connect  sys_ps8/emio_spi1_s_i GND
+
 ad_ip_parameter axi_sysid_0 CONFIG.ROM_ADDR_BITS 9
 ad_ip_parameter rom_sys_0 CONFIG.PATH_TO_FILE "[pwd]/mem_init_sys.txt"
 ad_ip_parameter rom_sys_0 CONFIG.ROM_ADDR_BITS 9
